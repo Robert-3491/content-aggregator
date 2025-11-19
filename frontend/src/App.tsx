86885ac0f21aa76add6fdd122cde7acd4c511391
rubynzar <rubynzar@yahoo.com>
2025-11-19 14:14:01 +0100

    one page app navigation implemented with smooth page transition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import AppNavigation from "./components/AppNavigation/AppNavigation";
 import Index from "./components/index/Index";
 import { AppProvider } from "./context/AppProvider";
+import styles from "@/styles/App.module.css";
 
 function App() {
+  const [navigationPage, setNavigationPage] = useState<"Browser" | "qBit">(
+    "Browser"
+  );
+
   return (
     <>
       <AppProvider>
-        <AppNavigation />
-        <Index />
+        <AppNavigation
+          navigationPage={navigationPage}
+          setNavigationPage={setNavigationPage}
+        />
+        <div className={styles.pageContainer}>
+          <div
+            className={`${styles.page} ${
+              navigationPage === "Browser" ? styles.active : ""
+            }`}
+          >
+            <Index />
+          </div>
+        </div>
       </AppProvider>
     </>
   );
--- frontend/src/components/AppNavigation/AppNavigation.tsx
+++ frontend/src/components/AppNavigation/AppNavigation.tsx
@@ -1,5 +1,32 @@
 import styles from "./AppNavigation.module.css";
 
-export default function AppNavigation() {
-  return <div>AppNavigation</div>;
+interface Props {
+  navigationPage: "Browser" | "qBit";
+  setNavigationPage: (selection: "Browser" | "qBit") => void;
+}
+
+export default function AppNavigation({
+  navigationPage,
+  setNavigationPage,
+}: Props) {
+  return (
+    <div className={styles.navigationContainer}>
+      <div
+        className={`${styles.navigationCell} ${
+          navigationPage === "Browser" ? styles.active : ""
+        }`}
+        onClick={() => setNavigationPage("Browser")}
+      >
+        Browser
+      </div>
+      <div
+        className={`${styles.navigationCell} ${
+          navigationPage === "qBit" ? styles.active : ""
+        }`}
+        onClick={() => setNavigationPage("qBit")}
+      >
+        qBit WebUI
+      </div>
+    </div>
+  );
 }
